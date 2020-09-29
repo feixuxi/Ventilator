a51db45854ccd47512eb28e99a820ac8682e7403
a-vinod <arjunvinod@umail.ucsb.edu>
2020-09-28 22:17:02 -0700

    typo fix

--- software/gui/gui.sh
+++ software/gui/gui.sh
@@ -145,7 +145,7 @@ if [ "$1" == "--build" ]; then
   qmake -unset QMAKEFEATURES
   git submodule update --init --recursive
 
-  if [ "$2" == "--no-checks" ] || [ "$3" == "--no-checks" ] || [ "$4" == "--no-checks" ] ;; then
+  if [ "$2" == "--no-checks" ] || [ "$3" == "--no-checks" ] || [ "$4" == "--no-checks" ]; then
     set +e
     set +o pipefail
   fi
